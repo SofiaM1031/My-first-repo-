d2de3a2844c6ff7e995c08ef4227253101e499de
SofiaM1031 <33524284+SofiaM1031@users.noreply.github.com>
2017-11-09 12:07:14 -0500

    Initial

---
+++ Mad_Libs_Sofia_Mackenzie.py
@@ -0,0 +1,102 @@
+print("Write a noun")
+noun1 = input ()
+
+print("write a noun")
+noun2 = input ()
+
+print("Write noun")
+noun3 = input ()
+
+print("Write adjective")
+adjective1 = input ()
+
+print("Write verb")
+verb1 = input ()
+
+print("Write noun")
+noun4 = input ()
+
+print("Write verb that ends in er")
+verb2 = input ()
+
+print("Write noun")
+noun5 = input ()
+
+print("Write noun")
+noun6 = input ()
+
+print("Write adjective")
+adjective2 = input ()
+
+print("Write verb")
+verb3 = input ()
+
+print("Write verb")
+verb4 = input ()
+
+print("Write verb")
+verb5 = input ()
+
+print("Write verb")
+verb6 = input ()
+
+print("Write verb")
+verb7 = input ()
+
+print("Write verb")
+verb8 = input ()
+
+print("Write verb")
+verb9 = input ()
+
+print("Write verb")
+verb10 = input ()
+
+print("Write noun")
+noun7 = input ()
+
+print("Write verb")
+verb11 = input ()
+
+print("Write noun")
+noun8 = input ()
+
+print("Write name")
+name1 = input ()
+
+print("I don't like your little " + noun1 +
+" Don't like your tilted " + noun2 +
+" The role you made me play " +
+"Of the " + noun3 + " , no, I don't like you " +
+" I don't like your " + adjective1 + " " + noun4 +
+" How you " + verb1 + " when you lie " +
+"You said the " + noun4 + " was mine " +
+"Isn't cool, no, I don't like you (oh!) " +
+"But I got" + verb2 + " , I got harder in the nick of time" +
+"Honey, I rose up from the" + noun5 + " , I do it all the time" +
+"I've got a " + noun6 + " of names and yours is in red," + adjective2 +
+"I" + verb3 +  "it once, then I " + verb3 + " it twice, oh! "  +
+
+"Ooh, look what you made me " + verb4 +
+"Look what you made me " + verb5 +
+"Look what you just made me " + verb6 +
+"Look what you just made me " +
+"Ooh, look what you made me " + verb7 +
+"Look what you made me " + verb8 +
+"Look what you just made me " + verb9 +
+"Look what you just made me " + verb10 +
+
+"I don't like your kingdom" + noun7 +
+"They once belonged to me" +
+"You ask me for a place to " + verb11 +
+"Locked me out and threw a feast (what?)" +
+"The world moves on, another day, another drama, drama" 
+"But not for me, not for me, all I think about is " + noun8 +
+"And then the world moves on, but one thing's for sure " +
+"Maybe I got mine, but you'll all get yours "
+
+
+"I'm sorry, the old" + name1 + " can't come to the phone right now" +
+"Why? " +
+"Oh, 'cause she's dead! (oh!)")
+
